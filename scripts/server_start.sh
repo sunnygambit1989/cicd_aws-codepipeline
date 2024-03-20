ef1864e4ebe0b1f49de065f34d9d1d07ebc12e50
sunny kumar <sunnygambit@gmail.com>
2024-03-21 00:54:39 +0530

    added target in  target/enablix-app.jar

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 cd /home/ec2-user/server/sunny
 sudo /usr/bin/java -jar -Dserver.port=80 \
-    enablix-app.jar > /dev/null 2> /dev/null < /dev/null &
+    target/enablix-app.jar > /dev/null 2> /dev/null < /dev/null &
